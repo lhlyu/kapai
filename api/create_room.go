75100d38a627b3fb579d36b183ce458b780e0273
lhlyu <misakafs@gmail.com>
2022-06-20 13:57:18 +0800

    update

--- api/create_room.go
+++ api/create_room.go
@@ -4,12 +4,45 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"strconv"
 	"sync"
 	"time"
 )
+
+var collection *mongo.Collection
+
+func init() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err != nil {
+		log.Panic(err)
+	}
+	if err = client.Ping(context.Background(), nil); err != nil {
+		log.Panic(err)
+	}
+	collection = client.Database(os.Getenv("MONGO_DB")).Collection(os.Getenv("MONGO_COL"))
+}
+
+type Player struct {
+	Id     string `json:"id" bson:"id"`
+	Name   string `json:"name" bson:"name"`
+	RoomId string `json:"roomId" bson:"roomId"`
+}
+
+type Room struct {
+	Id string `json:"id" bson:"id"`
+	// 1 - 等待; 2 - 游戏中
+	Status  int     `json:"status" bson:"status"`
+	Player1 *Player `json:"player1" bson:"player1"`
+	Player2 *Player `json:"player2" bson:"player2"`
+}
 
 var lock = sync.Mutex{}
 
--- api/get_room.go
+++ api/get_room.go
@@ -3,42 +3,8 @@ package api
 import (
 	"context"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"log"
 	"net/http"
-	"os"
-	"time"
 )
-
-var collection *mongo.Collection
-
-func init() {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
-	if err != nil {
-		log.Panic(err)
-	}
-	if err = client.Ping(context.Background(), nil); err != nil {
-		log.Panic(err)
-	}
-	collection = client.Database(os.Getenv("MONGO_DB")).Collection(os.Getenv("MONGO_COL"))
-}
-
-type Player struct {
-	Id     string `json:"id" bson:"id"`
-	Name   string `json:"name" bson:"name"`
-	RoomId string `json:"roomId" bson:"roomId"`
-}
-
-type Room struct {
-	Id string `json:"id" bson:"id"`
-	// 1 - 等待; 2 - 游戏中
-	Status  int     `json:"status" bson:"status"`
-	Player1 *Player `json:"player1" bson:"player1"`
-	Player2 *Player `json:"player2" bson:"player2"`
-}
 
 // GetRoom 获取房间信息
 func GetRoom(w http.ResponseWriter, r *http.Request) {
