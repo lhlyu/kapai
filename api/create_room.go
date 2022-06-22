aedf67f6e836fed4894b1f40fd7d9e4d438583c9
lhlyu <misakafs@gmail.com>
2022-06-23 03:16:36 +0800

    revert

--- api/create_room.go
+++ api/create_room.go
@@ -4,12 +4,31 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"strconv"
 	"sync"
 	"time"
 )
+
+var collection *mongo.Collection
+
+func init() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err != nil {
+		log.Panic(err)
+	}
+	if err = client.Ping(context.Background(), nil); err != nil {
+		log.Panic(err)
+	}
+	collection = client.Database(os.Getenv("MONGO_DB")).Collection(os.Getenv("MONGO_COL"))
+}
 
 type Player struct {
 	Id     string `json:"id" bson:"id"`
